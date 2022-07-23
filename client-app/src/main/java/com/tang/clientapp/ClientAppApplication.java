d09d2ab39aa7d5c6202f9ea7c3f7a930564a36d0
weepppp <zoeaki@163.com>
2022-07-23 10:48:14 +0800

    2

---
+++ client-app/src/main/java/com/tang/clientapp/ClientAppApplication.java
@@ -0,0 +1,13 @@
+package com.tang.clientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientAppApplication.class, args);
+    }
+
+}
